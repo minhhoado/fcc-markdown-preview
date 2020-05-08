dc16fe4a6f4c6b8c31d3dbecf216818f5e394e44
Do Tran Minh Hoa <minhhoa.do97@gmail.com>
2020-05-08 16:11:11 +0700

    Fully function markdown preview

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import marked from "marked";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com)
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+
+
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: initialState,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value,
+    });
+  };
+
+  render() {
+    const { text } = this.state;
+    const markdown = marked(text, { breaks: true });
+    return (
+      <div>
+        <h2 className="text-center mt-4">Convert your markdown</h2>
+        <div className="row">
+          <div className="col-6">
+            <textarea
+              className="form-control p-2"
+              id="editor"
+              cols="30"
+              rows="10"
+              value={text}
+              onChange={this.handleChange}
+              placeholder="Write your markdown"
+            ></textarea>
+          </div>
+          <div
+            className="col-6 preview rounded p-2"
+            dangerouslySetInnerHTML={{ __html: markdown }}
+            id="preview"
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
